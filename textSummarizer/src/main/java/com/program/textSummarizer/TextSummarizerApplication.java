4c8bd4c35e08e620a9ad5ce35a38e42dce1f4e6f
charish37 <charishmanadipalli77@gmail.com>
2025-11-03 21:27:02 -0500

    add textSummarizer

---
+++ textSummarizer/src/main/java/com/program/textSummarizer/TextSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.program.textSummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextSummarizerApplication.class, args);
+	}
+
+}
